9d66519d2a0df1e8ba1a94901e60007d6a78aca2
MichaelWiss <michael.wiss@gmail.com>
2019-11-04 15:04:55 -0600

    jsx review

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,8 +14,42 @@ var template = React.createElement(
 		'p',
 		null,
 		'This is some info'
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'Item one'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item two'
+		)
+	)
+);
+
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Michael W'
+	),
+	React.createElement(
+		'p',
+		null,
+		'Age:47'
+	),
+	React.createElement(
+		'p',
+		null,
+		'Location: Minnesota'
 	)
 );
 var appRoute = document.getElementById('app');
 
-ReactDOM.render(template, appRoute);
+ReactDOM.render(templateTwo, appRoute);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 console.log('App.js is running');
 
-	var template = <div><h1>Indecision App</h1>
-	<p>This is some info</p></div>;
+	var template = (
+	   <div>
+	     <h1>Indecision App</h1>
+	     <p>This is some info</p>
+	     <ol>
+	       <li>Item one</li>
+	       <li>Item two</li>
+	     </ol>
+	   </div>);
+
+	var templateTwo = (
+		<div>
+		  <h1>Michael W</h1>
+		  <p>Age:47</p>
+		  <p>Location: Minnesota</p>
+		</div>
+		);
     var appRoute = document.getElementById('app');
 
-ReactDOM.render(template, appRoute);
+ReactDOM.render(templateTwo, appRoute);
